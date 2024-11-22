eb21d38a0329f3bda855070560ebadc73bae819e
leonardo munoz 🌑🌍 <168220131+leonardomunooz@users.noreply.github.com>
2024-11-22 05:07:13 +0000

    Se ha hecho la estructura de la pagina profile

--- src/front/js/pages/Profile.jsx
+++ src/front/js/pages/Profile.jsx
@@ -2,6 +2,19 @@ import React  from "react";
 
 export const Profile = () => {
     return (
-        <h1>Profile Page</h1>
+        <div className="container">
+                <div className="row mt-5  p-4 ">
+                   <div className="m-auto col-8 ">
+                        <img className="m-auto d-block" src="https://placehold.co/200" alt="" />
+                   </div>
+                   <h1 className=" m-auto mt-3 col-8 text-center"> Welcome</h1>
+                
+                    <h4 className="text-center mt-3"> 
+                        <label htmlFor="apiKey"> Api Key : </label>
+                        <p className="d-inline-block border  p-1" id="apiKey" >AIzaSyDaGmWKa4JsXZ-HjGw7ISLn_3nSDAds4ewQe</p>
+                    </h4>
+                  
+                </div>
+        </div>
     )
 }
